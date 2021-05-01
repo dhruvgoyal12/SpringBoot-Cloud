a4714c642d615e9b5909d4a80565ce61a16d17d8
Dhruv <dhruv@rsocial.in>
2021-05-01 13:11:45 +0530

    first commit

---
+++ Restful_WebService/src/main/java/com/rest/webservices/Restful_WebService/RestfulWebServiceApplication.java
@@ -0,0 +1,25 @@
+package com.rest.webservices.Restful_WebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Restful Web Services
+//Social Media Application
+//Retrieve all Users -GET /users
+//Create a User - POST /users
+//Retrieve one User - GET /users/{id}
+//Delete a User - DELETE /users/{id}
+//
+//Retrieve all posts for a User - GET /users/{id}/posts
+//Create a post for a User - POST /users/{id}/posts
+//Retrieve details of a post -GET /users/{id}/posts/{post_id}
+
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ Restful_WebService/src/test/java/com/rest/webservices/Restful_WebService/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.Restful_WebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
