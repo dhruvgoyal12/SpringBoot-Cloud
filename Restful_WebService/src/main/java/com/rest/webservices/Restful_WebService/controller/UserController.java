abd067bec609cc4d9cbee6c9a8444f982fce5e88
Dhruv <dhruv@rsocial.in>
2021-05-01 22:19:39 +0530

    Enhanced User Create to return URI in headers

--- Restful_WebService/src/main/java/com/rest/webservices/Restful_WebService/controller/UserController.java
+++ Restful_WebService/src/main/java/com/rest/webservices/Restful_WebService/controller/UserController.java
@@ -3,8 +3,11 @@ package com.rest.webservices.Restful_WebService.controller;
 import com.rest.webservices.Restful_WebService.Bean.User;
 import com.rest.webservices.Restful_WebService.dao.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,9 +28,13 @@ public class UserController {
         return userDaoService.findOne(id);
     }
 
-    @PostMapping(path="users")
-    public void createUser(@RequestBody User user){
+    @PostMapping(path = "users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = userDaoService.save(user);
+        // Getting the uri for the created user
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser
+                .getId()).toUri();
 
+        return ResponseEntity.created(location).build();
     }
 }
