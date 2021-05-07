c8701a5f1f2687dbad1e04554d656aefd2181243
Dhruv <dhruv@rsocial.in>
2021-05-07 17:52:02 +0530

    Added h2 console configuration

--- Restful_WebService/src/main/java/com/rest/webservices/Restful_WebService/Bean/User.java
+++ Restful_WebService/src/main/java/com/rest/webservices/Restful_WebService/Bean/User.java
@@ -3,14 +3,20 @@ package com.rest.webservices.Restful_WebService.Bean;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "All details about User.")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @ApiModelProperty(notes = "Name should be between 1 and 50")
