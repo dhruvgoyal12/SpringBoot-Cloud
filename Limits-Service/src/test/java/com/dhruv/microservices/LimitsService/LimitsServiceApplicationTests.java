b2a872db28df06be18693c8266f2f4754dfcd872
Dhruv <dhruv@rsocial.in>
2021-05-10 12:13:44 +0530

    Starter project for limits microservice

---
+++ Limits-Service/src/test/java/com/dhruv/microservices/LimitsService/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.microservices.LimitsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
