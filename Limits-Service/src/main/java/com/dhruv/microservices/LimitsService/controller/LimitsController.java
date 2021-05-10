e9dc4e5ac96e0e03661793ee2a619673e110ebe7
Dhruv <dhruv@rsocial.in>
2021-05-10 13:19:08 +0530

    Added configuration

--- Limits-Service/src/main/java/com/dhruv/microservices/LimitsService/controller/LimitsController.java
+++ Limits-Service/src/main/java/com/dhruv/microservices/LimitsService/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.dhruv.microservices.LimitsService.controller;
 
 import com.dhruv.microservices.LimitsService.bean.Limits;
+import com.dhruv.microservices.LimitsService.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits getLimits(){
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
