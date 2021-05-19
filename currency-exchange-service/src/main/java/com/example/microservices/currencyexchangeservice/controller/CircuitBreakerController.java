044f7a06d52a7442b5c2d31b32aebbb3aab700c5
Dhruv <dhruv@rsocial.in>
2021-05-19 22:23:10 +0530

    Explored ratelimiter and bulkhead

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.example.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +17,11 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name= "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name= "default", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name= "default", fallbackMethod = "hardcodedResponse")
+    //@RateLimiter(name="default")
+    // 10s => 10000 calls to the sample api is done through rate limiting
+
+    @Bulkhead(name="default")
     public String sampleApi(){
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url", String.class);
